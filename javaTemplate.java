2e94747d99e0f12bb25f271bce9cb2f3047f53f2
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2019-04-30 02:09:22 +0700

    Create javaTemplate.java

---
+++ javaTemplate.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+/******************************\
+ * The solution is at the top *
+ *                            *
+ *   Created by : azhar556    *
+\******************************/
+
+public class Solution {
+    static void solve() {
+        
+    }    
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        int t;// = ni();
+        t = 1;
+        while (t-- > 0) solve(); 
+        err.println("Time elapsed : " + (System.currentTimeMillis() - time) / 1000F + " s.");
+        err.close(); out.close();
+    }
+
+    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static PrintWriter err = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.err)));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer token;
+        
+    static String ns() {
+        while (token == null || !token.hasMoreTokens()) {
+            try {
+                token = new StringTokenizer(br.readLine());
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return token.nextToken();
+    }
+    static char nc() {
+        return Character.valueOf(ns().charAt(0));
+    }
+    static int ni() {
+        return Integer.parseInt(ns());
+    }
+    static double nd() {
+        return Double.parseDouble(ns());
+    }
+    static long nl() {
+        return Long.parseLong(ns());
+    }
+}
