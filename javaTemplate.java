4924b7b4e185d105f91fe6e6a21a48875fd6bdd2
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2020-03-24 08:18:19 +0700

    Update javaTemplate.java

--- javaTemplate.java
+++ javaTemplate.java
@@ -17,7 +17,7 @@ public class Solution {
         int t;// = ni();
         t = 1;
         while (t-- > 0) solve(); 
-        err.println("\nTime elapsed : " + (System.currentTimeMillis() - time) / 1000F + " s.");
+        err.println("\nTime elapsed : " + (System.currentTimeMillis() - time) / 1000F + " s.\n");
         err.close(); out.close();
     }
 
