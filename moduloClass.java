611f0032c9111f8b6266bb7dfedaf075cdd1174e
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2024-08-08 15:29:12 +0700

    Update moduloClass.java

--- moduloClass.java
+++ moduloClass.java
@@ -1,27 +1,24 @@
 static class Modulo {
 		static int modInverse(int A, int M) { // A = number, M = modulo
-			int g = gcd(A, M);
+			
 			return power(A, M - 2, M);
 		}
 
 		static int power(int x, int y, int M) {
-			if (y == 0) {
-				return 1;
-			}
-			int p = power(x, y / 2, M) % M;
-			p = (int)((p * (long)p) % M);
-			if (y % 2 == 0) {
-				return p;
-			}
-			else {
-				return (int)((x * (long)p) % M);
-			}
-		}
-
-		static int gcd(int a, int b) {
-			if (a == 0) {
-				return b;
-			}
-			return gcd(b % a, a);
+			long xx = (long)x;
+			xx %= M;
+		    if (xx == 0) return 0;
+		    long ret = 1L;
+		    while (y > 0){
+		        if (y % 2 == 1){  
+		            ret *= xx;
+		            ret %= M; 
+		            y--;
+		        }
+		        xx *= xx; 
+		        xx %= M; 
+		        y >>= 1;
+		    }
+		    return (int)ret;
 		}
 	}
